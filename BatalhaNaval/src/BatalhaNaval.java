10643eb3bbc175801e3660a5cc45274643a4dac7
desireex <desireex51@gmail.com>
2024-08-26 18:43:06 -0300

    comentarios

--- BatalhaNaval/src/BatalhaNaval.java
+++ BatalhaNaval/src/BatalhaNaval.java
@@ -14,6 +14,7 @@ public class BatalhaNaval extends JFrame{
     JPanel quadrado = new JPanel();
     JLabel pontos = new JLabel("Pontuação: 0");
     public BatalhaNaval(){
+        //construtor que inicia a interface grafica do jogo
         setVisible(true);
         setTitle("Batalha Naval");
         setDefaultCloseOperation(3);
@@ -22,8 +23,10 @@ public class BatalhaNaval extends JFrame{
     }
 
     public static void main(String[] args) throws IOException {
+        //instanciando objeto do tipo Batalha Naval e criação dos campoos de jogo
         BatalhaNaval j = new BatalhaNaval();
         j.CriaCampos();
+        //instanciando objeto do tipo cliente para iniciar a conexao
         Cliente c = new Cliente();
         c.Comunicacao();
         /*int[] bomba = j.MinhaVez();
@@ -45,11 +48,14 @@ public class BatalhaNaval extends JFrame{
 
     public int[][] InicioJogo(){
         QuadradinhoCentral("Posicione os seus Barcos");
+        //chamado de funcao que permite que o usuario posicione seus barcos
         AcionaHover(0);
+        //retorna o local em que foram posicionados barcos para enviar ao servidor
         return posicaoBarcos;
     }
 
     private void CriaCampos(){
+        //criacao do campo do jogador
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
                 meuCampo[i][j] = new JButton();
@@ -60,6 +66,7 @@ public class BatalhaNaval extends JFrame{
         JLabel texto1 = new JLabel("Meu mapa");
         texto1.setBounds(15, 5, 100, 25); // x, y, largura, altura
         add(texto1);
+        //criacao do campo que corresponde ao do oponente
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
                 CamposBatalha[i][j] = new JButton();
@@ -70,11 +77,12 @@ public class BatalhaNaval extends JFrame{
         JLabel texto2 = new JLabel("Mapa inimigo");
         texto2.setBounds(815, 5, 100, 25); // x, y, largura, altura
         add(texto2);
+        //criacao de um quadro central para passar informacoes ao jogador
         quadrado.setBounds(715, 370,95,95);
-        quadrado.setBackground(Color.blue);
+        quadrado.setBackground(Color.PINK);
         quadrado.setVisible(true);
         add(quadrado);
-        QuadradinhoCentral("Inicio do jogo!");
+        QuadradinhoCentral("Inicio do jogo :)!");
         pontos.setBounds(1400, 5, 100, 25); // x, y, largura, altura
         add(pontos);
     }
@@ -84,7 +92,8 @@ public class BatalhaNaval extends JFrame{
     }
 
     public void paint(Graphics g) {
-        super.paint(g);  // fixes the immediate problem.
+        //implementa linhas no centro da tela para dividir os mapas
+        super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
         Line2D lin = new Line2D.Float(770, 0, 770, 400);
         Line2D lin2 = new Line2D.Float(770, 495, 770, 845);
@@ -93,23 +102,29 @@ public class BatalhaNaval extends JFrame{
     }
 
     private void QuadradinhoCentral(String dito){
+        //modifica o texto central mostrado ao usuario
         JLabel textocentral = new JLabel();
         textocentral.setText(dito);
         quadrado.add(textocentral);
     }
 
     private void AcionaHover(int contaCliques) {
+        //instrucoes quanto a rotacao no posicionamento dos barcos
         JLabel instrucao = new JLabel("Aperte 'R' para mudar a orientação do barco");
         instrucao.setBounds(15, 750, 700, 25); // x, y, largura, altura
         add(instrucao);
+        //tamanhos dos barcos padrao de batalha naval
         int[] tamanhobarcos = {5, 4, 3, 3, 2};
+        //condicional que finaliza a recursiviade da funcao, indicando o fim do posicionamento
         if (contaCliques >= tamanhobarcos.length) {
             remove(instrucao);
             printaBarcos();
             return;}
         int size = tamanhobarcos[contaCliques];
-        ImageIcon emptyIcon = new ImageIcon(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB));
+        //variavel que controla a orientação do barco que o jogador quer posicionar
         final boolean[] horizontal = {true};
+        //imagens utilizadas
+        ImageIcon emptyIcon = new ImageIcon(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB));
         Image img = null;
         try {
             img = ImageIO.read(Objects.requireNonNull(getClass().getResource("barco.jpeg")));
@@ -117,7 +132,7 @@ public class BatalhaNaval extends JFrame{
             throw new RuntimeException(ex);
         }
         Icon imagemBarco = new ImageIcon(img);
-
+        //implementa a rotacao do barco ao pressionar a tecla R
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -126,9 +141,8 @@ public class BatalhaNaval extends JFrame{
                 }
             }
         });
-
         requestFocusInWindow();
-
+        //criacao que variaveis que permitem a interacao com os botoes
         MouseAdapter[][] mouseAdapters = new MouseAdapter[10][10];
         ActionListener[][] actionListeners = new ActionListener[10][10];
 
@@ -136,7 +150,7 @@ public class BatalhaNaval extends JFrame{
             for (int j = 0; j < 10; j++) {
                 int finalI = i;
                 int finalJ = j;
-
+                //funcao que permite o efeito de hover sobre os botoes
                 mouseAdapters[i][j] = new MouseAdapter() {
                     @Override
                     public void mouseEntered(MouseEvent e) {
@@ -163,6 +177,7 @@ public class BatalhaNaval extends JFrame{
                 if(meuCampo[i][j].isEnabled()) {
                     meuCampo[i][j].addMouseListener(mouseAdapters[i][j]);
                 }
+                //funcao que permite o posicionamento dos barcos no campo escolhido
                 actionListeners[i][j] = new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -178,7 +193,7 @@ public class BatalhaNaval extends JFrame{
                                 meuCampo[finalI][finalJ + pos].setDisabledIcon(imagemBarco);
                             }
                         }
-                        // Remover os MouseListeners
+                        // remove os MouseListeners
                         for (int h = 0; h < 10; h++) {
                             for (int g = 0; g < 10; g++) {
                                 meuCampo[h][g].removeMouseListener(mouseAdapters[h][g]);
@@ -186,9 +201,11 @@ public class BatalhaNaval extends JFrame{
                             }
                         }
                         posicaoBarcos[contaCliques] = barcos;
+                        //chamada recursiva da funcao indicando a quantidade de barcos posicionados
                         AcionaHover(contaCliques + 1);
                     }
                 };
+                //remove os ActionListeners
                 if (((horizontal[0] && finalI + size <= 10)||(!horizontal[0] && finalJ + size <= 10))&&meuCampo[i][j].isEnabled()) {
                     meuCampo[i][j].addActionListener(actionListeners[i][j]);
                 }
@@ -198,13 +215,16 @@ public class BatalhaNaval extends JFrame{
 
     public int[] MinhaVez() {
         QuadradinhoCentral("Sua vez");
+        //variavel que guarda a coordenada da bomba escolhida
         int[] botaoClicado = new int[2];
+        //criacao que variavel que permite a interacao com os botoes
         ActionListener[][] actionListeners = new ActionListener[10][10];
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 int finalJ = j;
                 int finalI = i;
                 actionListeners[i][j] = new ActionListener() {
+                    //funcao que permite a escolha do local para posicionar a bomba
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         botaoClicado[0] = finalI;
@@ -222,6 +242,7 @@ public class BatalhaNaval extends JFrame{
                                 if(finalI!=h || finalJ!=g) {
                                     CamposBatalha[h][g].setEnabled(false);
                                 }
+                                //remove as funcoes dos botoes
                                 CamposBatalha[h][g].removeActionListener(actionListeners[h][g]);
                             }
                         }
@@ -235,6 +256,7 @@ public class BatalhaNaval extends JFrame{
         return botaoClicado;
     }
     public void ProcessaBomba(int[] bomba, boolean acerto){
+        //desativa o local da bomba e ativa os outros
         CamposBatalha[bomba[0]][bomba[1]].setEnabled(false);
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
@@ -243,11 +265,13 @@ public class BatalhaNaval extends JFrame{
                 }
             }
         }
+        //verifica se o jogador acertou a bomba
         if(acerto){
             QuadradinhoCentral("Acertou!!!");
         }
         else{
             QuadradinhoCentral("Errou:(");
+            //adiciona imagem de x se errou
             Image img = null;
             try {
                 img = ImageIO.read(Objects.requireNonNull(getClass().getResource("xis.png")));
@@ -264,6 +288,7 @@ public class BatalhaNaval extends JFrame{
 
     public void ProcessaBombardeio(int[] bombaAdv){
         String foto;
+        //processa o bombardeio que o jogador recebeu do adversario e coloca uma imagem correspondente ao acerto
         if(meuCampo[bombaAdv[0]][bombaAdv[1]].isEnabled()){
             meuCampo[bombaAdv[0]][bombaAdv[1]].setEnabled(false);
             foto = "xis.png";
@@ -280,13 +305,14 @@ public class BatalhaNaval extends JFrame{
     }
 
     public void FimDeJogo(boolean venceu){
+        //indica se o jogador ganhou ou perdeu no fim do jogo
         if(venceu){
             QuadradinhoCentral("Você venceu!!!");
         }else{
             QuadradinhoCentral("Você perdeu :(");
         }
         try {
-            Thread.sleep(2000); // Espera 2 segundos antes de limpar
+            Thread.sleep(5000); // espera 5 segundos antes de limpar
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -295,6 +321,7 @@ public class BatalhaNaval extends JFrame{
         // Limpando todos os componentes do JFrame
         getContentPane().removeAll();
         repaint();
+        //indica que o o jogador feche o jogo
         JLabel tchau = new JLabel("Fim de jogo, feche a página");
         tchau.setBounds(100, 400, 1000, 50);
         add(tchau);
--- BatalhaNaval/src/Cliente.java
+++ BatalhaNaval/src/Cliente.java
@@ -9,26 +9,34 @@ public class Cliente {
     String HOST = "localhost";
     int PORT = 44444;
     private Socket cliente;
-    private DataInputStream in;
-    private DataOutputStream out;
+    private final DataInputStream in;
+    private final DataOutputStream out;
     BatalhaNaval jogo = new BatalhaNaval();
     int[][] posicaoBarcos = new int[5][4];
 
     public Cliente() throws IOException{
+        //construtor estabelece a conexao com o host a partir do IP e da Porta
         Socket cliente = new Socket(HOST, PORT);
-        DataInputStream in = new DataInputStream(cliente.getInputStream());
-        DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+        //instanciadas variaveis que permitem a leitura e escrita de dados primitivos
+        in = new DataInputStream(cliente.getInputStream());
+        out = new DataOutputStream(cliente.getOutputStream());
     }
 
     public void Comunicacao() throws IOException {
         int[] bomba = new int[2];
         String leitura = in.readUTF();
         do{
+            //filtra o tipo de sinal recebido pelo servidor
             switch (leitura) {
+                //sinal de conexao
                 case "A" -> ChecaConexao();
+                //sinal que filtra de quem é a vez
                 case "B" -> bomba = ProcessaVez();
+                //sinal que indica se o jogador acertou o barco do adversario
                 case "C" -> ProcessaAcerto(bomba);
+                //sinal que recebe a bomba posicionada pelo adversario
                 case "D" -> ProcessaAcertoAdv();
+                //sinal de vitoria
                 case "E" -> Vitoria();
             }
         }while (!in.readUTF().equals("bye"));
@@ -36,9 +44,12 @@ public class Cliente {
     }
 
     private void ChecaConexao() throws IOException {
+        //apos sinal de conexao, ve se foi estabelecida a conexao dos dois clientes
         if(in.readBoolean()){
             System.out.println("Conexao estabelecida");
+            //se a conexao foi estabelecida, inicia o jogo com o posicionamento dos barcos
             posicaoBarcos = jogo.InicioJogo();
+            //envia barcos posicionados para o server
             EnviaBarcos();
         }
         else{
@@ -52,6 +63,7 @@ public class Cliente {
 
     private int[] ProcessaVez() throws IOException {
         int[] bomba = new int[2];
+        //verifica se é a vez do jogador ou do adversario
         if(in.readBoolean()){
             bomba = jogo.MinhaVez();
             EnviaBomba(bomba);
@@ -61,10 +73,12 @@ public class Cliente {
         return bomba;
     }
     private void ProcessaAcerto(int[] bomba) throws IOException {
+        //verifica se o o jogador acertou e recebe a sua pontuação no momento
         jogo.ProcessaBomba(bomba, in.readBoolean());
         jogo.ControlaScore(in.readInt());
     }
     public void EnviaBarcos() throws IOException {
+        //envia coordenadas dos barcos posicionados
         out.writeInt(posicaoBarcos.length);  // Número de linhas
         out.writeInt(posicaoBarcos[0].length);  // Número de colunas
 
@@ -76,11 +90,13 @@ public class Cliente {
     }
     private void ProcessaAcertoAdv() throws IOException {
         int[] bombaAdv = new int[2];
+        //processa bomba do adversario
         bombaAdv[0] = in.readInt();
         bombaAdv[1] = in.readInt();
         jogo.ProcessaBombardeio(bombaAdv);
     }
     private void Vitoria() throws IOException {
+        //indica se o jogador ganhou
         jogo.FimDeJogo(in.readBoolean());
     }
     private void FechaConexao() throws IOException {
